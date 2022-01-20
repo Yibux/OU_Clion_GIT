24ba8eb92b326a19106373a72ddc7d0040e32174
Yibux <240810@edu.p.lodz.pl>
2022-01-20 13:39:55 +0100

    Zmiana w policz policz

--- main.c
+++ main.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
-int Policz(int a,int b)
-{
-    return a*b;
+int Policz (int a, int b) {
+    return (a+b)*(a-b);
 }
-
 int main() {
     int a=2,b=3;
-    printf("W pierwszych branchu; %d * %d = %d",a,b, Policz(a,b));
+    printf("W pierwszych branchu; (%d + %d)*(%d - %d) = %d",a,b,a,b, Policz(a,b));
     return 0;
 }
