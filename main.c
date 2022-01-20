75d07f604d0e1fa34cda29ca21e106ab8be05b62
Yibux <240810@edu.p.lodz.pl>
2022-01-20 13:19:33 +0100

    Nowa metoda Policz

--- main.c
+++ main.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+int Policz(int a,int b)
+{
+    return a+b;
+}
 
 int main() {
-    printf("Hello, World!\n");
+    int a=2,b=3;
+    printf("W pierwszych branchu; %d + %d = %d",a,b, Policz(a,b));
     return 0;
 }
