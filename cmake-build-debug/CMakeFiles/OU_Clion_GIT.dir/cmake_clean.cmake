a6fabf2f059558ea283ae851e63690b464e3f37c
Yibux <240810@edu.p.lodz.pl>
2022-01-20 13:16:35 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/OU_Clion_GIT.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OU_Clion_GIT.dir/main.c.obj"
+  "OU_Clion_GIT.exe"
+  "OU_Clion_GIT.exe.manifest"
+  "OU_Clion_GIT.pdb"
+  "libOU_Clion_GIT.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/OU_Clion_GIT.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
