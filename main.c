a4174c35b8d3b15cf6809effca0a21d0c34a8387
Yibux <240810@edu.p.lodz.pl>
2022-01-20 13:44:37 +0100

    Zmiana w policz policz

--- main.c
+++ main.c
@@ -4,6 +4,6 @@ int Policz (int a, int b) {
 }
 int main() {
     int a=2,b=3;
-    printf("W pierwszych branchu; (%d + %d)*(%d - %d) = %d",a,b,a,b, Policz(a,b));
+    printf("(%d + %d)*(%d - %d) = %d",a,b,a,b, Policz(a,b));
     return 0;
 }
